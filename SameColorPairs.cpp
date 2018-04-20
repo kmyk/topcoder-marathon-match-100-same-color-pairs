958cf1fba027bb94691c892443888a77bc1c9a41
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-04-20 16:25:01 +0900

    something like simulated annealing

--- SameColorPairs.cpp
+++ SameColorPairs.cpp
@@ -45,30 +45,51 @@ inline double rdtsc() { // in seconds
 constexpr int TLE = 10; // sec
 double clock_begin;
 
-bool is_valid_pair(vector<vector<int> > const & board, pair<int, int> const & p1, pair<int, int> const & p2) {
+inline array<int, 4> to_array(pair<int, int> const & p1, pair<int, int> const & p2) {
+    return (array<int, 4>) { p1.first, p1.second, p2.first, p2.second };
+}
+inline array<int, 4> to_array(pair<pair<int, int>, pair<int, int> > const & p12) {
+    return to_array(p12.first, p12.second);
+}
+
+bool is_valid_pair(vector<vector<int> > const & board, pair<int, int> const & p1, pair<int, int> const & p2, pair<int, int> & cont) {
     int y1, x1; tie(y1, x1) = p1;
     int y2, x2; tie(y2, x2) = p2;
     int c = board[y1][x1];
     if (y1 > y2) swap(y1, y2);
     if (x1 > x2) swap(x1, x2);
-    REP3 (y, y1, y2 + 1) {
-        REP3 (x, x1, x2 + 1) {
+    int & y = cont.first;
+    int & x = cont.second;
+    if (y == -1) y = y1;
+    if (x == -1) x = x1;
+    for (; y < y2 + 1; ++ y) {
+        for (; x < x2 + 1; ++ x) {
             if (board[y][x] != -1 and board[y][x] != c) {
                 return false;
             }
         }
+        x = x1;
     }
     return true;
 }
 
-void use_pair(vector<vector<int> > & board, vector<pair<int, int> > & color, int i, int j, vector<array<int, 4> > & rects) {
-    int y1, x1; tie(y1, x1) = color[i];
-    int y2, x2; tie(y2, x2) = color[j];
+inline bool is_valid_pair(vector<vector<int> > const & board, pair<int, int> const & p1, pair<int, int> const & p2) {
+    pair<int, int> cont = { -1, -1 };
+    return is_valid_pair(board, p1, p2, cont);
+}
+
+void use_pair(vector<vector<int> > & board, pair<int, int> const & p1, pair<int, int> const & p2) {
+    int y1, x1; tie(y1, x1) = p1;
+    int y2, x2; tie(y2, x2) = p2;
+    board[y1][x1] = -1;
+    board[y2][x2] = -1;
+}
+void use_pair(vector<vector<int> > & board, pair<int, int> const & p1, pair<int, int> const & p2, vector<array<int, 4> > & rects) {
+    int y1, x1; tie(y1, x1) = p1;
+    int y2, x2; tie(y2, x2) = p2;
     rects.push_back((array<int, 4>) { y1, x1, y2, x2 });
     board[y1][x1] = -1;
     board[y2][x2] = -1;
-    swap(color[i], color.back()); color.pop_back();
-    swap(color[j], color.back()); color.pop_back();
 }
 
 vector<array<int, 4> > solve_prepare(int H, int W, int C, vector<vector<int> > & board, int limit) {
@@ -90,10 +111,12 @@ vector<array<int, 4> > solve_prepare(int H, int W, int C, vector<vector<int> > &
     while ((int)rects.size() < limit) {
         bool found = false;
         REP (c, C) {
-            REP_R (ci, color[c].size()) {  // use backward
-                REP (cj, ci) {
-                    if (not is_valid_pair(board, color[c][ci], color[c][cj])) continue;
-                    use_pair(board, color[c], ci, cj, rects);
+            REP_R (i, color[c].size()) {  // use backward
+                REP (j, i) {
+                    if (not is_valid_pair(board, color[c][i], color[c][j])) continue;
+                    use_pair(board, color[c][i], color[c][j], rects);
+                    swap(color[c][i], color[c].back()); color[c].pop_back();
+                    swap(color[c][j], color[c].back()); color[c].pop_back();
                     found = true;
                     goto next_color;
                 }
@@ -106,30 +129,108 @@ next_color: ;
 }
 
 template <class RandomEngine>
-vector<array<int, 4> > solve1(int H, int W, int C, vector<vector<int> > board, vector<array<int, 4> > rects, RandomEngine & gen) {
-    vector<vector<pair<int, int> > > color(C);
-    REP (y, H) REP (x, W) if (board[y][x] != -1) {
-        color[board[y][x]].emplace_back(y, x);
-    }
-    REP (c, C) {
-        shuffle(ALL(color[c]), gen);
-    }
-    while (true) {
-        bool found = false;
-        REP (c, C) {
-            REP (ci, color[c].size()) {
-                REP (cj, ci) {
-                    if (not is_valid_pair(board, color[c][ci], color[c][cj])) continue;
-                    use_pair(board, color[c], ci, cj, rects);
-                    found = true;
-                    goto next_color;
+vector<int> make_order(int H, int W, vector<vector<int> > & board, vector<pair<pair<int, int>, pair<int, int> > > const & pairs, RandomEngine & gen) {
+    vector<int> result;
+    map<pair<int, int>, vector<int> > depended;
+    vector<pair<int, int> > cont(pairs.size(), make_pair(-1, -1));
+    function<void (int)> go = [&](int i) {
+        auto const & p1 = pairs[i].first;
+        auto const & p2 = pairs[i].second;
+        if (is_valid_pair(board, p1, p2, cont[i])) {
+            use_pair(board, p1, p2);
+            result.push_back(i);
+            for (auto pi : { p1, p2 }) {
+                auto it = depended.find(pi);
+                if (it == depended.end()) continue;
+                for (int j : it->second) {
+                    go(j);
                 }
+                it->second = vector<int>();  // relase
             }
-next_color: ;
+        } else {
+            depended[cont[i]].push_back(i);
         }
-        if (not found) break;
+    };
+    REP (i, pairs.size()) {
+        go(i);
     }
-    return rects;
+    return result;
+}
+
+template <class RandomEngine>
+vector<array<int, 4> > solve1(int H, int W, int C, vector<vector<int> > const & original_board, RandomEngine & gen) {
+#ifdef LOCAL
+    int already_removed = 0;
+    REP (y, H) REP (x, W) {
+        already_removed += original_board[y][x] == -1;
+    }
+#endif
+    vector<vector<int> > board = original_board;
+    vector<array<int, 4> > best_rects;
+    vector<array<int, 4> > rects;
+
+    int iteration = 0;
+    for (; ; ++ iteration) {
+        double t = (rdtsc() - clock_begin) / TLE;
+        if (t > 0.95) break;
+
+        vector<pair<pair<int, int>, pair<int, int> > > pairs;
+        vector<vector<pair<int, int> > > color(C);
+        REP (y, H) REP (x, W) if (board[y][x] != -1) {
+            color[board[y][x]].emplace_back(y, x);
+        }
+        REP (c, C) {
+            shuffle(ALL(color[c]), gen);
+        }
+        while (true) {
+            bool found = false;
+            REP (c, C) {
+                int k = color[c].size();
+                if (k == 0) continue;
+                found = true;
+                pairs.emplace_back(color[c][k - 1], color[c][k - 2]);
+                color[c].pop_back();
+                color[c].pop_back();
+            }
+            if (not found) break;
+        }
+        vector<int> result = make_order(H, W, board, pairs, gen);
+        for (int i : result) {
+            rects.push_back(to_array(pairs[i]));
+        }
+
+        if (rects.size() > best_rects.size()) {
+            best_rects = rects;
+#ifdef LOCAL
+            cerr << "score = " << (already_removed + 2 * best_rects.size()) /(double) (H * W) << endl;
+#endif
+        }
+
+        if (result.empty()) {
+            double r = uniform_real_distribution<double>()(gen);
+            int k = uniform_int_distribution<int>(0, r < 0.00001 ? 1000 : r < 0.001 ? 100 : 2)(gen);
+            while (k -- and not rects.empty()) {
+                int y1 = rects.back()[0];
+                int x1 = rects.back()[1];
+                int y2 = rects.back()[2];
+                int x2 = rects.back()[3];
+                rects.pop_back();
+                board[y1][x1] = original_board[y1][x1];
+                board[y2][x2] = original_board[y2][x2];
+            }
+        }
+    }
+
+    cerr << "iteration = " << iteration << endl;
+    return best_rects;
+}
+
+template <class T>
+vector<T> vector_concat(vector<T> const & a, vector<T> const & b) {
+    vector<T> c;
+    copy(ALL(a), back_inserter(c));
+    copy(ALL(b), back_inserter(c));
+    return c;
 }
 
 vector<array<int, 4> > solve(int H, int W, int C, vector<vector<int> > const & a_board) {
@@ -148,35 +249,10 @@ vector<array<int, 4> > solve(int H, int W, int C, vector<vector<int> > const & a
         cerr << endl;
     }
 #endif
-
-    vector<array<int, 4> > result;
     random_device device;
     xor_shift_128 gen(device());
-    int iteration = 0;
-    double last_t = (rdtsc() - clock_begin) / TLE;
-    double max_delta = 0;
-    for (; ; ++ iteration) {
-
-        double t = (rdtsc() - clock_begin) / TLE;
-        if (t + 2 * max_delta + 0.05 > 0.50) break;
-        vector<array<int, 4> > it = solve1(H, W, C, board, base, gen);
-        if (result.size() < it.size()) {
-            result = it;
-            cerr << "score = " << (result.size() /(double) (H * W / 2)) << endl;
-            if (int(result.size()) == H * W / 2) {
-                ++ iteration;
-                break;  // the optimal
-            }
-        }
-        chmax(max_delta, t - last_t);
-        last_t = t;
-    }
-
-    cerr << "iteration = " << iteration << endl;
-    double t = (rdtsc() - clock_begin) / TLE;
-    cerr << "time = " << t << endl;
-    cerr << "max delta = " << max_delta << endl;
-    return result;
+    vector<array<int, 4> > result = solve1(H, W, C, board, gen);
+    return vector_concat(base, result);
 }
 
 class SameColorPairs {
@@ -225,6 +301,8 @@ public:
             oss << rect[3];
             answer.push_back(oss.str());
         }
+        double t = (rdtsc() - clock_begin) / TLE;
+        cerr << "time = " << t << endl;
         return answer;
     }
 };
